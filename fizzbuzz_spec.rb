c93d78f1814549fcffea6c763911febebee4ddfe
shgtkshruch <shgtk.shruch@gmail.com>
2014-04-24 23:17:33 +0900

    Refactor fizzbuzz_spec.rb

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -3,41 +3,23 @@ require 'rspec'
 require_relative './fizzbuzz'
 
 describe FizzBuzz do
-  subject { FizzBuzz.new }
+  subject {FizzBuzz.new}
 
-  context '与えられた数字が１のとき' do
-    it '１を返すこと' do
+  context '与えられた数字が３の倍数でも５の倍数でもないとき' do
+    it '与えられた数字を返すこと' do
       subject.say(1).should == 1
     end
   end
 
-  context '与えられた数字が２のとき' do
-    it '２を返すこと' do
-      subject.say(2).should == 2
-    end
+  context '与えられた数字が３の倍数のとき' do
+    it {subject.say(3).should == 'Fizz'}
   end
 
-  context '与えられた数字が３のとき' do
-    it 'Fizzを返すこと' do
-      subject.say(3).should == 'Fizz'
-    end
+  context '与えられた数字が５の倍数のとき' do
+    it {subject.say(5).should == 'Buzz'}
   end
 
-  context '与えられた数字が５のとき' do
-    it 'Buzzを返すこと' do
-      subject.say(5).should == 'Buzz'
-    end
-  end
-
-  context '与えられた数字が９のとき' do
-    it 'Fizzを返すこと' do
-      subject.say(9).should == 'Fizz'
-    end
-  end
-
-  context '与えられた数字が１５のとき' do
-    it 'FizzBuzzを返すこと' do
-      subject.say(15).should == 'FizzBuzz'
-    end
+  context '与えられた数字が３の倍数かつ５の倍数のとき' do
+    it {subject.say(15).should == 'FizzBuzz'}
   end
 end
